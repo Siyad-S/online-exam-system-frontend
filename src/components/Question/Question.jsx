3dce26650bfa6b597c71e186cf95695817b147c5
Siyad-S <siyadsaidu313@gmail.com>
2024-03-15 16:10:03 +0530

    third commit

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,40 +1,60 @@
-// Question.js
-
 import React, { useEffect, useState } from "react";
 import "./Question.css";
 import { useFormik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 import { getQuestion } from "../../redux/Slices/question";
 import { checkUserMark } from "../../redux/Slices/user";
+import { current } from "@reduxjs/toolkit";
 
 const Question = () => {
   const dispatch = useDispatch();
   const allQuestions = useSelector((state) => state.questions.all.data);
   const [currentPage, setCurrentPage] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [submitButton, setSubmitButton] = useState(false);
 
   useEffect(() => {
-    dispatch(getQuestion({ page: currentPage, pageSize: allQuestions?.length }));
+    dispatch(getQuestion({ page: currentPage, pageSize }));
   }, []);
+
+  const pageSize = allQuestions?.length;
 
   const totalPages = allQuestions ? allQuestions.length : 0;
 
   const formik = useFormik({
     initialValues: {
-      answer: "", 
-      questionId: "", 
+      [`answer${currentPage}`]: "",
+      [`questionId${currentPage}`]: ""
     },
     onSubmit: (values) => {
-      const questionId = allQuestions[currentPage]._id;
-      dispatch(checkUserMark({ questionId, answers: values }));
+      // dispatch(checkUserMark({questionId, answers: values}))
     },
   });
 
+  console.log(formik.values);
+
+
   const handleNext = () => {
     if (currentPage < totalPages - 1) {
+      setSelectedAnswers([
+        ...selectedAnswers,
+        formik.setFieldValue(
+          `questionId${currentPage}`,
+          allQuestions[currentPage]._id,
+          formik.values[`answer${currentPage}`]
+        ),
+      ]);
       setCurrentPage(currentPage + 1);
     }
   };
+
+  const submitBtnFunction = () => {
+    formik.setFieldValue(
+      `questionId${currentPage}`,
+      allQuestions[currentPage]._id,
+      formik.values[`answer${currentPage}`]
+    )
+  }
 
   const handlePrevious = () => {
     if (currentPage > 0) {
@@ -52,18 +72,22 @@ const Question = () => {
             <h4>{allQuestions[currentPage].question}</h4>
             <p>Please select your answer:</p>
             <div className="options">
-              {allQuestions[currentPage]?.answers?.map((answer, answerIndex) => (
-                <label key={answerIndex}>
-                  <input
-                    type="radio"
-                    name="answer"
-                    value={answer.answer}
-                    onChange={formik.handleChange}
-                    checked={formik.values.answer === answer.answer}
-                  />
-                  {answer.answer}
-                </label>
-              ))}
+              {allQuestions[currentPage]?.answers?.map(
+                (answer, answerIndex) => (
+                  <label key={answerIndex}>
+                    <input
+                      type="radio"
+                      name={`answer${currentPage}`}
+                      value={answer.answer}
+                      onChange={formik.handleChange}
+                      checked={
+                        formik.values[`answer${currentPage}`] === answer.answer
+                      }
+                    />
+                    {answer.answer}
+                  </label>
+                )
+              )}
             </div>
           </div>
           <div className="buttons">
@@ -75,10 +99,11 @@ const Question = () => {
             >
               Previous
             </button>
-            <button
+
+     <button
               type={currentPage === totalPages - 1 ? "submit" : "button"}
               id="nextBtn"
-              onClick={handleNext}
+              onClick={currentPage !== totalPages - 1 ? handleNext : submitBtnFunction}
             >
               {currentPage !== totalPages - 1 ? "Next" : "Submit Exam"}
             </button>
